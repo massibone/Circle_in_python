bd370036849a61ac08fb936b3955917abed3c02b
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2023-03-19 20:52:53 +0100

    Update check_circle_intersection.py

--- check_circle_intersection.py
+++ check_circle_intersection.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 print("Input x1, y1, r1, x2, y2, r2:")
 x1,y1,r1,x2,y2,r2 = 1,1,4,1,1,5#[int(i) for i in input().split()]
 d = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
 if d <= r1-r2:
     print("C2  is in C1")
 elif d <= r2-r1:
@@ -16,9 +17,9 @@ elif d == r1+r2:
 else:
     print("C1 and C2  do not overlap")
 
-figure, axes = plt.subplots()
-colored_circle =plt.Circle((x1, x2), r1, color='blue')
-axes.set_aspect( 1 )
-axes.add_artist( colored_circle )
-plt.title( 'Colored Circle' )
+#figure, axes = plt.subplots()
+#colored_circle =plt.Circle((x1, x2), r1, color='blue')
+#axes.set_aspect( 1 )
+#axes.add_artist( colored_circle )
+#plt.title( 'Colored Circle' )
 #plt.show()
